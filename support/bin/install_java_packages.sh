33aa3b8aa8ff39f0a11452b7be4227a116046485
Sam Hough <sam@redspr.com>
2024-06-29 18:04:07 +0100

    more debug

--- support/bin/install_java_packages.sh
+++ support/bin/install_java_packages.sh
@@ -8,6 +8,10 @@ currentDir=`pwd`
 cd `dirname $0`/.. && topDir=`pwd`
 binDir=$topDir/bin
 
+echo "XXXXXXXXXXXXXXXXXXXXXXX before java2"
 $binDir/install_java.sh || exit 1
+echo "XXXXXXXXXXXXXXXXXXXXXXX before ant"
 $binDir/install_ant.sh || exit 1
+echo "XXXXXXXXXXXXXXXXXXXXXXX before glassfish"
 $binDir/install_glassfish.sh || exit 1
+echo "XXXXXXXXXXXXXXXXXXXXXXX before after glassfish"
